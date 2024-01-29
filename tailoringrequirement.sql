33ec5747c24b7d2a3a591ae21ee0cd269ba1c751
Michael Bädorf <53299725+baedorf@users.noreply.github.com>
2024-01-29 13:29:27 +0100

    Select a requirement of a dedicated project tailoring

---
+++ tailoringrequirement.sql
@@ -0,0 +1,23 @@
+WITH RECURSIVE
+tr AS (
+    SELECT CHAPTER_ID, PARENTCHAPTER_ID, NAME 
+  	    FROM TAILORINGCATALOGCHAPTER 
+  	    WHERE CHAPTER_ID = (
+  	        SELECT CHAPTER_ID 
+  	            FROM TAILORINGCATALOG 
+  	            WHERE CATALOG_ID = (
+  	                SELECT TAILORINGCATALOG_ID 
+  	                    FROM TAILORING 
+  	                    WHERE NAME = :NAME AND PROJECT_ID = (
+  	                        SELECT PROJECT_ID 
+  	                            FROM PROJECT 
+  	                            WHERE IDENTIFIER = :IDENTIFIER
+  	                    )
+  	            )
+        )
+    UNION ALL
+    SELECT t.CHAPTER_ID, t.PARENTCHAPTER_ID, t.NAME 
+  	    FROM TAILORINGCATALOGCHAPTER t
+  	    JOIN tr ON t.PARENTCHAPTER_ID = tr.CHAPTER_ID
+)
+Select * from TAILORINGREQUIREMENT where NUMBER = :NUMBER and CHAPTER_ID in (select CHAPTER_ID from tr)
